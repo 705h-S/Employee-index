ed08f9889c88def5641ff1125efbd0cbffd28f19
705H-S <93358442+705h-S@users.noreply.github.com>
2022-02-02 19:05:39 -0800

    added basic values to tables in seeds

--- db/schema.sql
+++ db/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE roles(
 
 -- third table named employees
 CREATE TABLE employees(
-    id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id  INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,29 @@
-INSERT
+-- inserting values into deparment table
+INSERT INTO department (name)
+VALUES ("Human Resources"),
+       ("Finance"),
+       ("Legal"),
+       ("Production"),
+       ("Customer Service"),
+       ("Engineering");
+
+-- Inserting values into Roles table
+INSERT INTO roles (title, salary)
+VALUES ("Recruiter",40000.00),
+       ("Accountant",74000.00),
+       ("Lawyer",87000.00),
+       ("Assembler",27000.00),
+       ("Support",30000.00),
+       ("Software Engineer",100000.00);
+
+-- Inserting values into employees table
+INSERT INTO employees (first_name, last_name)
+VALUES ("Alexis", "Frost"),
+       ("Emily", "Straw"),
+       ("John", "Snow"),
+       ("Barry", "Allen"),
+       ("Daniela", "Olivares"),
+       ("Bruce", "Wayne");
+
+SELECT * FROM employees_db.deparment;
+
